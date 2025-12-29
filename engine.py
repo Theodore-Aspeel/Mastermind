e019cc84f45dcd6a49230ac4a86b73c26771e44a
Theodore <theo.aspeel@gmail.be>
2025-12-29 10:36:54 +0100

    CORRECT secrete_code_selection function

--- engine.py
+++ engine.py
@@ -18,11 +18,11 @@ CODE_LENGTH = 4
 
 def secrete_code_selection() -> List[str]:
     """
-    Select randomly the secret colour code that the player will have to guess
+    Choose randomly colours to the code that the player will have to guess
     """
     selected_colour = []
     for _ in range(CODE_LENGTH):
         # Add a random color to the end of an existing list
-        selected_colour.append(random(COLORS))
+        selected_colour.append(choice(COLORS))
     return selected_colour
 print(secrete_code_selection())

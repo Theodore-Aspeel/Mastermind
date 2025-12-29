5ba0f5180216af194dde6a74795ce8b5cd387a5a
Theodore <theo.aspeel@gmail.be>
2025-12-29 11:09:50 +0100

    Add guess_test function (not fonctional).

--- engine.py
+++ engine.py
@@ -16,7 +16,7 @@ CODE_LENGTH = 4
 
 # --------------------------Fonctions--------------------------
 
-def secrete_code_selection() -> List[str]:
+def secrete_code() -> List[str]:
     """
     Choose randomly colours to the code that the player will have to guess
     """
@@ -25,4 +25,25 @@ def secrete_code_selection() -> List[str]:
         # Add a random color to the end of an existing list
         selected_colour.append(choice(COLORS))
     return selected_colour
-print(secrete_code_selection())
+# print(secrete_code())
+
+"""def player_selection() -> List[str]:
+    colour_guess = []
+    for _ in range(CODE_LENGTH):
+        colour_guess.append(choice(COLORS))
+    return colour_guess
+    
+    PAS DE SELECTION ICI MAIS DANS L UI DIRECTEMENT
+    """
+
+def guess_test():
+
+    well_placed = 0
+    for n in range(CODE_LENGTH):
+        if player_selection(n) == secrete_code(n):
+            well_placed += 1
+
+    good_colour = 0
+    for _ in range(CODE_LENGTH):
+        if
+
